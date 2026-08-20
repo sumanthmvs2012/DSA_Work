f60d233a6237d740e311c74f13c83e01bb2c69c2
venkatamvs <venkatamvs90@gmail.com>
2026-08-20 13:29:58 +0530

    Update NonOverlappingIntervals.java

--- src/Day_10/NonOverlappingIntervals.java
+++ src/Day_10/NonOverlappingIntervals.java
@@ -31,7 +31,7 @@ class eraseOverlapIntervals {
             {
                 prev = i;
                 count++;
-                //sumanth
+                //changes
             }
         }
         return n - count;
